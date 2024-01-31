ea9ef5414148f6e90bb2dc7b3831ef4aecb7cbb7
Olena Abert <oabert@gmail.com>
2024-01-31 16:17:15 -0500

    Add admin page,add display menu

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_CATEGORY
 
 
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by('-date_created')  # if add '-' receive reverse order
     template_name = 'index.html'
 
-    def get_context_data(self):
-        contex = {'meals': 'Pizza'}
+    def get_context_data(self, **kwargs):
+        contex = super().get_context_data(**kwargs)
+        # contex = {'meals':MEAL_CATEGORY}
+        contex['meals'] = MEAL_CATEGORY
         return contex
 
 
